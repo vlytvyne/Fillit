8485f0428b14cf5499baecf458fed4f4a2b7a88a
Oleksandr KRYZHANOVSKYI <okryzhan@e2r11p17.unit.ua>
2018-11-07 14:34:31 +0200

    sfillit i think done

--- sfillit.c
+++ sfillit.c
@@ -6,9 +6,19 @@ int		is_place(char **board, char **shape, int i, int j)
 	int i2;
 	int j2;
 	int tmp;
+	static int count;
 
 	i2 = 0;
 	tmp = j;
+	count++;
+	if (shape[0][0] == EMPTY)
+	{
+		//printf("i : %d, j : %d\n", i, j);
+		//ft_print_board(board);
+		tmp -= 1;
+		// for (int k = 0; k < 2; k++)
+			// ft_putendl(shape[k]);
+	}
 	while (shape[i2])
 	{
 		j = tmp;
@@ -16,7 +26,13 @@ int		is_place(char **board, char **shape, int i, int j)
 		while (shape[i2][j2])
 		{
 			if (shape[i2][j2] != EMPTY && board[i][j] != EMPTY)
+			{
+				if (count == 14)
+				{
+					printf("TYT = i : %d, j : %d\n\n", i, j);
+				}
 				return (0);
+			}
 			j++;
 			j2++;
 		}
@@ -24,31 +40,6 @@ int		is_place(char **board, char **shape, int i, int j)
 		i2++;
 	}
 	return (1);
-}
-
-void	put(char **board, char **shape, int i, int j)
-{
-	int i2;
-	int j2;
-	int tmp;
-
-	i2 = 0;
-	tmp = j;
-	printf("i : %d, j : %d\n", i, j);
-	while (shape[i2])
-	{
-		j = tmp;
-		j2 = 0;
-		while (shape[i2][j2])
-		{
-			if (shape[i2][j2] != EMPTY)
-				board[i][j] = shape[i2][j2];
-			j++;
-			j2++;
-		}
-		i++;
-		i2++;
-	}
 }
 
 void	clear(char **board, char **shape, int i, int j)
@@ -59,14 +50,15 @@ void	clear(char **board, char **shape, int i, int j)
 
 	i2 = 0;
 	tmp = j;
-	printf("i : %d, j : %d\n", i, j);
+	if (shape[0][0] == EMPTY)
+			tmp -= 1;
 	while (shape[i2])
 	{
 		j = tmp;
 		j2 = 0;
 		while (shape[i2][j2])
 		{
-			if (shape[i2][j2] == BLOCK)
+			if (shape[i2][j2] != EMPTY)
 				board[i][j] = EMPTY;
 			j++;
 			j2++;
@@ -76,35 +68,58 @@ void	clear(char **board, char **shape, int i, int j)
 	}
 }
 
+char	**put(char **board, t_block *elem, int i, int j, t_block *list)
+{
+	int i2;
+	int j2;
+	int tmp;
+
+	i2 = 0;
+	tmp = j;
+	if (elem->shape[0][0] == EMPTY)
+			tmp -= 1;
+	while (elem->shape[i2])
+	{
+		j = tmp;
+		j2 = 0;
+		while (elem->shape[i2][j2])
+		{
+			if (elem->shape[i2][j2] != EMPTY)
+				board[i][j] = elem->shape[i2][j2];
+			j++;
+			j2++;
+		}
+		i++;
+		i2++;
+	}
+	return (ft_fillit(board, elem->next, list));
+}
+
 char	**ft_fillit(char **board, t_block *elem, t_block *list)
 {
 	int i;
 	int j;
 
 	i = 0;
-	if (elem == NULL) // Если блоки закончились - мы их все поставили - Ура
+	if (elem == NULL)
 		return (board);
 	while (i <= g_size - elem->height)
 	{
 		j = 0;
 		while (j <= g_size - elem->width)
 		{
-			if (board[i][j] == '.') // Если не удалось поставить - пробуем другую клетку
+			if (board[i][j] == '.' && is_place(board, elem->shape, i, j))
 			{
-				if (is_place(board, elem->shape, i, j))
-				{
-					printf("is_place - ok\n");
-					put(board, elem->shape, i, j);
-					return (ft_fillit(board, elem->next, list));
-				}
-				else
-				{
-					printf("is_place - ko\n");
-				}
+					if (put(board, elem, i, j, list) == NULL)
+						clear(board, elem->shape, i, j);
+					else
+						return (board);
 			}
 			j++;
 		}
 		i++;
-	} 
-	return (ft_fillit(ft_update_board(board), list, list)); // Если все клетки закончились увеличиваем размер доски
+	}
+	if (elem == list)
+		return (ft_fillit(ft_update_board(board), elem, list));
+	return (NULL);
 }
